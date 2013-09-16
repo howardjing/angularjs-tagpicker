1047313e3f4fd6eb943234eeb3417b3330418c97
Howard Jing <howard.jing@gmail.com>
2013-09-16 03:21:40 -0400

    adding preliminary autocomplete support

--- src/tagpicker.js
+++ src/tagpicker.js
@@ -1,121 +1,243 @@
-angular.module('tagpicker', [])
-
-// display existing tags and the new tag
-.directive('tagpicker', [function() {
-  return {
-    restrict: 'E',
-    replace: true,
-    templateUrl: "../src/tagpicker.html",
-    scope: {
-      tags: '='
-    },
-    controller: ['$scope', '$attrs', '$controller', function($scope, $attrs, $controller) {
-      var self = this;
-
-      $scope.tags = $scope.tags || [];
-      $scope.currentTag = '';
-      $scope.focused = false;
-
-      var inArray = function(array, element) {
-        return array.indexOf(element) != -1;
-      }
-
-      $scope.deleteTag = function(tag) {
-        if (inArray($scope.tags, tag)) {
-          $scope.tags.splice($scope.tags.indexOf(tag), 1);
-        }
-      }
-
-      $scope.triggerFocus = function() {
-        self.setFocus(true);
-      }
-
-      this.createNewTag = function() {
-        // don't allow empty tag and don't allow duplicate tags
-        if ($scope.currentTag && !inArray($scope.tags, $scope.currentTag)) {
-          $scope.tags.push($scope.currentTag);
-          $scope.currentTag = '';
-        }
-      }
-
-      this.isNewTag = function() {
-        return $scope.currentTag == '';
-      }
-
-      this.isFocused = function() {
-        return $scope.focused;
-      }
-
-      this.setFocus = function(value) {
-        $scope.focused = value;
-      }
-
-      // different from $scope.deleteTag because
-      // this will only delete the last tag
-      // while deleteTag will search for a tag to delete
-      this.deleteLastTag = function() {
-        if ($scope.tags.length > 0) {
-          $scope.tags.pop();
-        }
-      }
-    }]
+(function() {
+  // TODO: should probably extract this into a config setting
+  var minChars = 3;
+  var hasValidTag = function(currentTag) {
+    return currentTag && currentTag.length >= minChars;
   }
-}])
 
-// input for entering new tag
-.directive('taginput', [function() {
+  angular.module('tagpicker', [])
 
-  // pressing enter or comma will make a new tag
-  var ENTER = 13;
-  var COMMA = 188;
+  // display existing tags and the new tag
+  .directive('tagpicker', ['$timeout', function($timeout) {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: "../src/tagpicker.html",
+      scope: {
+        tags: '=',
+        autocomplete: '&'
+      },
+      controller: ['$scope', '$attrs', '$controller', 
+      function($scope, $attrs, $controller) {
+        var self = this;
 
-  // pressing this while empty will delete a previous tag
-  var DELETE = 8;
-  return {
-    restrict: 'A',
-    require: '^tagpicker',
-    link: function(scope, elem, attrs, tagsPicker) {
+        $scope.tags = $scope.tags || [];
+        $scope.currentTag = '';
+        $scope.focused = false;
 
-      scope.$watch(function() { return tagsPicker.isFocused(); }, function(isFocused) {
-        if (isFocused) {
-          elem[0].focus();
-        }
-      })
-      
-      elem.bind('keydown', function(e) {
-        if (e.keyCode == ENTER || e.keyCode == COMMA) {
-          e.preventDefault();
-          scope.$apply(function() {
-            tagsPicker.createNewTag();
-          });
+        var inArray = function(array, element) {
+          return array.indexOf(element) != -1;
         }
 
-        if (e.keyCode == DELETE && tagsPicker.isNewTag()) {
-          scope.$apply(function() {
-            e.preventDefault();
-            tagsPicker.deleteLastTag();
-          });
+        $scope.deleteTag = function(tag) {
+          if (inArray($scope.tags, tag)) {
+            $scope.tags.splice($scope.tags.indexOf(tag), 1);
+          }
         }
-      });
 
-      elem.bind('focus', function(e) {
-        // will already be applying if this element was focused
-        // as a result of the tagsPicker:focused event
-        if (!tagsPicker.isFocused()) {
-          scope.$apply(function() {
-            tagsPicker.setFocus(true);
-          })
+        $scope.triggerFocus = function() {
+          self.setFocus(true);
         }
-      });
 
-      elem.bind('blur', function(e) {
-        if (tagsPicker.isFocused()) {
-          scope.$apply(function() {
-            tagsPicker.setFocus(false);
-            tagsPicker.createNewTag();
-          })
+        this.createNewTag = function(newTag) {
+          // don't allow empty tag and don't allow duplicate tags
+          if (hasValidTag(newTag) && !inArray($scope.tags, newTag)) {
+            $scope.tags.push(newTag);
+            $scope.currentTag = '';
+          }
         }
-      });
+        $scope.createNewTag = self.createNewTag;
+
+        this.isNewTag = function() {
+          return $scope.currentTag == '';
+        }
+
+        this.isFocused = function() {
+          return $scope.focused;
+        }
+
+        this.setFocus = function(value) {
+          $scope.focused = value;
+        }
+
+        // different from $scope.deleteTag because
+        // this will only delete the last tag
+        // while deleteTag will search for a tag to delete
+        this.deleteLastTag = function() {
+          if ($scope.tags.length > 0) {
+            $scope.tags.pop();
+          }
+        }
+      }]
     }
-  }
-}])
+  }])
+
+  // internal directive for entering new tag
+  .directive('tagInput', [function() {
+
+    // pressing enter or comma will make a new tag
+    var ENTER = 13;
+    var COMMA = 188;
+
+    // pressing this while empty will delete a previous tag
+    var DELETE = 8;
+
+    // clear the current tag input
+    var ESCAPE = 27;
+
+    // move between selections
+    var UP_ARROW = 38;
+    var DOWN_ARROW = 40;
+
+    return {
+      restrict: 'A',
+      require: ['^tagpicker', '^tagAutocomplete'],
+      link: function(scope, elem, attrs, ctrls) {
+        var tagpicker = ctrls[0];
+        var autocomplete = ctrls[1];
+
+        scope.$watch(function() { 
+          return tagpicker.isFocused(); 
+        }, function(isFocused) {
+          if (isFocused) {
+            elem[0].focus();
+          }
+        })
+        
+        // should probably just use the ng-keydown directive at this point
+        var handleKeydown = function(e, onKeydown) {
+          e.preventDefault();
+          scope.$apply(onKeydown);
+        }
+
+        scope.selectResult = autocomplete.selectResult;
+        scope.getSelection = autocomplete.getSelection;
+
+        elem.bind('keydown', function(e) {
+          // user wants to create a new tag
+          if (e.keyCode == ENTER || e.keyCode == COMMA) {
+            handleKeydown(e, function() {
+              tagpicker.createNewTag(
+                autocomplete.getSelection() || scope.currentTag
+              );            
+            })
+          } 
+
+          // user wants to delete the last tag
+          if (e.keyCode == DELETE && tagpicker.isNewTag()) {
+            handleKeydown(e, function() {
+              tagpicker.deleteLastTag();
+            });
+          } 
+
+          // user wants to delete the current tag input
+          if (e.keyCode == ESCAPE) {
+            handleKeydown(e, function() {
+              scope.currentTag = '';
+            })
+          }
+
+          // user wants to select the previous result          
+          if (e.keyCode == UP_ARROW) {
+            handleKeydown(e, function() {
+              autocomplete.selectPrevious();
+            });
+          } 
+
+          // user wants to select the next result          
+          if (e.keyCode == DOWN_ARROW) {
+            handleKeydown(e, function() {
+              autocomplete.selectNext();
+            });
+          } 
+        });
+
+        elem.bind('focus', function(e) {
+          // will already be applying if this element was focused
+          // as a result of the tagpicker:focused event
+          if (!tagpicker.isFocused()) {
+            scope.$apply(function() {
+              tagpicker.setFocus(true);
+            })
+          }
+        });
+
+        elem.bind('blur', function(e) {
+          if (tagpicker.isFocused()) {
+            scope.$apply(function() {
+              tagpicker.setFocus(false);
+              tagpicker.createNewTag(scope.currentTag);
+            })
+          }
+        });
+      }
+    }
+  }])
+
+  // internal directive for handling autocompletion
+  .directive('tagAutocomplete', function() {
+    return {
+      restrict: 'A',
+      controller: ['$scope', function(scope) {
+        var self = this;
+        var minChars = 3;
+        var hasAutocomplete = !!scope.autocomplete();
+
+        if (hasAutocomplete) {
+
+          scope.$watch('currentTag', function(value) {
+            if (!value) {
+              scope.selectedTagIndex = 0;
+            }
+          })
+
+          // query autocomplete function appropriately
+          scope.$watch('currentTag', function(value) {
+            if (hasValidTag(value)) {
+              scope.results = scope.autocomplete()(value);
+            } else {
+              scope.results = [];
+            }
+          })
+
+          // insert current user input into results appropriately
+          scope.$watchCollection('results', function(results) {
+            if (hasValidTag(scope.currentTag) && 
+              results.indexOf(scope.currentTag) == -1) {
+              scope.results.unshift(scope.currentTag);
+            }
+          })
+        }
+
+        // defining controller methods
+        if (hasAutocomplete) {
+          this.getSelection = function() {
+            return scope.results[scope.selectedTagIndex];
+          }
+
+          this.selectPrevious = function() {
+            if (scope.selectedTagIndex > 0) {
+              scope.selectedTagIndex -= 1;
+            }
+          }
+
+          this.selectNext = function() {
+            if (scope.selectedTagIndex < scope.results.length -1) {
+              scope.selectedTagIndex += 1;
+            }
+          }
+
+          this.selectResult = function(result) {
+            scope.selectedTagIndex = scope.results.indexOf(result);
+          }
+        } else { 
+          // tagAutocomplete must implement these methods no matter what
+          controllerMethods = ['getSelection', 'selectPrevious', 'selectNext'];
+          angular.forEach(controllerMethods, function(method) {
+            self[method] = angular.noop;
+          })
+        }
+      }]
+    }
+  })
+})()
