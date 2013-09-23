b2ebd308521cc2694082fbfe5ccfa5c40b69b92c
Howard Jing <howard.jing@gmail.com>
2013-09-23 02:22:20 -0400

    adding jshint grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js']
+    }
+  });
+};
--- src/tagpicker.js
+++ src/tagpicker.js
@@ -3,12 +3,12 @@
   var minChars = 2;
   var hasValidTag = function(currentTag) {
     return currentTag && currentTag.length >= minChars;
-  }
+  };
 
   angular.module('tagpicker', [])
 
   // display existing tags and the new tag
-  .directive('tagpicker', ['$timeout', function($timeout) {
+  .directive('tagpicker', [function() {
     return {
       restrict: 'E',
       replace: true,
@@ -17,8 +17,7 @@
         tags: '=',
         autocomplete: '&'
       },
-      controller: ['$scope', '$attrs', '$controller', 
-      function($scope, $attrs, $controller) {
+      controller: ['$scope', function($scope) {
         var self = this;
 
         $scope.tags = $scope.tags || [];
@@ -26,18 +25,18 @@
         $scope.focused = false;
 
         var inArray = function(array, element) {
-          return array.indexOf(element) != -1;
-        }
+          return array.indexOf(element) !== -1;
+        };
 
         $scope.deleteTag = function(tag) {
           if (inArray($scope.tags, tag)) {
             $scope.tags.splice($scope.tags.indexOf(tag), 1);
           }
-        }
+        };
 
         $scope.triggerFocus = function() {
           self.setFocus(true);
-        }
+        };
 
         this.createNewTag = function(newTag) {
           // don't allow empty tag and don't allow duplicate tags
@@ -45,20 +44,20 @@
             $scope.tags.push(newTag);
             $scope.currentTag = '';
           }
-        }
+        };
         $scope.createNewTag = self.createNewTag;
 
         this.isNewTag = function() {
-          return $scope.currentTag == '';
-        }
+          return $scope.currentTag === '';
+        };
 
         this.isFocused = function() {
           return $scope.focused;
-        }
+        };
 
         this.setFocus = function(value) {
           $scope.focused = value;
-        }
+        };
 
         // different from $scope.deleteTag because
         // this will only delete the last tag
@@ -67,9 +66,9 @@
           if ($scope.tags.length > 0) {
             $scope.tags.pop();
           }
-        }
+        };
       }]
-    }
+    };
   }])
 
   // internal directive for entering new tag
@@ -102,76 +101,76 @@
           if (isFocused) {
             elem[0].focus();
           }
-        })
+        });
         
         // should probably just use the ng-keydown directive at this point
         var handleKeydown = function(e, onKeydown) {
           e.preventDefault();
           scope.$apply(onKeydown);
-        }
+        };
 
         scope.selectResult = autocomplete.selectResult;
         scope.getSelection = autocomplete.getSelection;
 
         elem.bind('keydown', function(e) {
           // user wants to create a new tag
-          if (e.keyCode == ENTER || e.keyCode == COMMA) {
+          if (e.keyCode === ENTER || e.keyCode === COMMA) {
             handleKeydown(e, function() {
               tagpicker.createNewTag(
                 autocomplete.getSelection() || scope.currentTag
               );            
-            })
+            });
           } 
 
           // user wants to delete the last tag
-          if (e.keyCode == DELETE && tagpicker.isNewTag()) {
+          if (e.keyCode === DELETE && tagpicker.isNewTag()) {
             handleKeydown(e, function() {
               tagpicker.deleteLastTag();
             });
           } 
 
           // user wants to delete the current tag input
-          if (e.keyCode == ESCAPE) {
+          if (e.keyCode === ESCAPE) {
             handleKeydown(e, function() {
               scope.currentTag = '';
-            })
+            });
           }
 
           // user wants to select the previous result          
-          if (e.keyCode == UP_ARROW) {
+          if (e.keyCode === UP_ARROW) {
             handleKeydown(e, function() {
               autocomplete.selectPrevious();
             });
           } 
 
           // user wants to select the next result          
-          if (e.keyCode == DOWN_ARROW) {
+          if (e.keyCode === DOWN_ARROW) {
             handleKeydown(e, function() {
               autocomplete.selectNext();
             });
           } 
         });
 
-        elem.bind('focus', function(e) {
+        elem.bind('focus', function() {
           // will already be applying if this element was focused
           // as a result of the tagpicker:focused event
           if (!tagpicker.isFocused()) {
             scope.$apply(function() {
               tagpicker.setFocus(true);
-            })
+            });
           }
         });
 
-        elem.bind('blur', function(e) {
+        elem.bind('blur', function() {
           if (tagpicker.isFocused()) {
             scope.$apply(function() {
               tagpicker.setFocus(false);
               tagpicker.createNewTag(scope.currentTag);
-            })
+            });
           }
         });
       }
-    }
+    };
   }])
 
   // internal directive for handling autocompletion
@@ -180,10 +179,7 @@
       restrict: 'A',
       controller: ['$scope', function(scope) {
         var self = this;
-        var minChars = 3;
         var hasAutocomplete = !!scope.autocomplete();
-
-
 
         if (hasAutocomplete) {
 
@@ -193,7 +189,7 @@
             if (!value) {
               scope.selectedTagIndex = 0;
             }
-          })
+          });
 
           // query autocomplete function appropriately
           scope.$watch('currentTag', function(value) {
@@ -202,46 +198,46 @@
             } else {
               scope.results.data = [];
             }
-          })
+          });
 
           // insert current user input into results appropriately
           scope.$watchCollection('results.data', function(results) {
             if (hasValidTag(scope.currentTag) && 
-              results.indexOf(scope.currentTag) == -1) {
+              results.indexOf(scope.currentTag) === -1) {
               results.unshift(scope.currentTag);
             }
-          })
+          });
         }
 
         // defining controller methods
         if (hasAutocomplete) {
           this.getSelection = function() {
             return scope.results.data[scope.selectedTagIndex];
-          }
+          };
 
           this.selectPrevious = function() {
             if (scope.selectedTagIndex > 0) {
               scope.selectedTagIndex -= 1;
             }
-          }
+          };
 
           this.selectNext = function() {
             if (scope.selectedTagIndex < scope.results.data.length -1) {
               scope.selectedTagIndex += 1;
             }
-          }
+          };
 
           this.selectResult = function(result) {
             scope.selectedTagIndex = scope.results.data.indexOf(result);
-          }
+          };
         } else { 
           // tagAutocomplete must implement these methods no matter what
-          controllerMethods = ['getSelection', 'selectPrevious', 'selectNext'];
+          var controllerMethods = ['getSelection', 'selectPrevious', 'selectNext'];
           angular.forEach(controllerMethods, function(method) {
             self[method] = angular.noop;
-          })
+          });
         }
       }]
-    }
-  })
-})()
+    };
+  });
+})();
