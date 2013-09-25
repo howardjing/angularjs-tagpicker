4879e032c7012d2dcf8c1c33dfb35ec62fad6488
Howard Jing <howard.jing@gmail.com>
2013-09-25 01:54:57 -0400

    Adding grunt-html2js task
    For converting the tagpicker template into a js template.
    Also editing bower.json to use angular-mocks as a devDependency.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,24 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-html2js');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    html2js: {
+      dist: {
+        options: {
+          module: null,
+          base: '.'
+        },
+        files: [{
+          expand: true,
+          src: ['templates/**/*.html'],
+          ext: '.html.js'
+        }]
+      }
     }
   });
 };
