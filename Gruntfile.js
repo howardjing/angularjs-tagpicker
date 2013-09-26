ea56128b2cad1f63d8e99a78246a952b497955d7
Howard Jing <howard.jing@gmail.com>
2013-09-25 23:33:51 -0400

    Adding grunt watch task
    For running jshint and karma unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,21 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-html2js');
 
 
-  grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('build', ['jshint', 'clean', 'html2js']);
+  grunt.registerTask('default', ['karma:unit', 'watch']);
+  grunt.registerTask('build', ['clean', 'html2js']);
 
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      files: ['src/**/*.js'],
+      tasks: ['jshint', 'karma:unit:run']
+    },
     jshint: {
       files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
     },
@@ -22,6 +28,14 @@ module.exports = function(grunt) {
         src: ['templates/**/*.html'],
         dest: 'dist/tagpicker-templates.html.js',
         module: 'tagpicker.templates'
+      }
+    },
+    karma: {
+      options: {
+        configFile: 'karma.conf.js',
+      },
+      unit: {
+        background: true
       }
     }
   });
