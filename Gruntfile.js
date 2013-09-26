f62ad13a24f4608ca2248d371712444908dbd58d
Howard Jing <howard.jing@gmail.com>
2013-09-25 22:54:01 -0400

    Adding clean and build tasks
    - build task doesn't really do anything yet
    - modifying html2js to output html template to dist folder,
      as well as handle multiple html templates appropriately

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,29 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-html2js');
+
+
+  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('build', ['jshint', 'clean', 'html2js']);
+
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
     },
+    clean: ['dist/'],
     html2js: {
       dist: {
         options: {
-          module: null,
           base: '.'
         },
-        files: [{
-          expand: true,
-          src: ['templates/**/*.html'],
-          ext: '.html.js'
-        }]
+        src: ['templates/**/*.html'],
+        dest: 'dist/tagpicker-templates.html.js',
+        module: 'tagpicker.templates'
       }
     }
   });
+
 };
